9ba5bae2f530a7a4c40174deb311978415ca6cfe
BertVerbouw <bert_verbouw#hotmail.be>
2015-04-20 23:48:24 +0200

    first commit

---
+++ js/site.js
@@ -0,0 +1,5 @@
+function setMail(leiding){
+    
+        document.getElementById(leiding).innerHTML = '<a href="mailto:'+leiding+'leiding@scoutsessencentrum.be">'+leiding+'leiding@scoutsessencentrum.be';
+    
+}
